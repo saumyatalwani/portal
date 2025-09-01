0810c08963d123e5c4dd7dc2d0f94fbd6d3942c5
Saumya Talwani <68903741+saumyatalwani@users.noreply.github.com>
2025-09-01 14:18:47 +0530

    try

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
+
+// Use require to avoid TS errors
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
