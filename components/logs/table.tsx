a2dcc52cab5e2215402b5db737c57d594587d13b
Saumya Talwani <68903741+saumyatalwani@users.noreply.github.com>
2025-09-01 14:03:40 +0530

    final try

--- components/logs/table.tsx
+++ components/logs/table.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/common/DataTable";
 import { ArrowUpDown } from "lucide-react";
 import { ColumnDef } from "@tanstack/react-table";
-import { logs } from "@/generated/prisma";
+import { logs } from "@/app/generated/prisma";
 import React from "react";
 
 export default function LogTable({data}: {
