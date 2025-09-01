915a2733c2fcff9e064aedc92812da84d10a03bd
Saumya Talwani <68903741+saumyatalwani@users.noreply.github.com>
2025-09-01 14:41:44 +0530

    undo comment

--- app/api/student/route.ts
+++ app/api/student/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
-//import { auth } from "@/lib/auth";
-//import prisma from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
 
 export async function GET(req: Request) {
-  //const headers = req.headers;
+  const headers = req.headers;
   
-  //const session = await auth.api.getSession({ headers });
-  //if (!session) {return NextResponse.json({ success: false, error: "Unauthorized" }, { status: 401 });}
+  const session = await auth.api.getSession({ headers });
+  if (!session) {return NextResponse.json({ success: false, error: "Unauthorized" }, { status: 401 });}
   
-  //const list = await prisma.student.findMany();
+  const list = await prisma.student.findMany();
   return NextResponse.json({ data : [] },{ status: 200 });
 }
